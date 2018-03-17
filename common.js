ca72e21a6633b3c61cc3cf7d4f15509258175fd5
lyrieek <tianhao7@foxmail.com>
2018-03-17 23:33:53 +0800

    install vue&jquery

---
+++ common.js
@@ -0,0 +1,9 @@
+(function( global) {
+  //something
+}(typeof window !== "undefined" ? window : this);
+
+$(function () {
+    Vue.component('app-header', {
+      template: '<div style="font-size:24px;">Header</div>'
+    });
+});
